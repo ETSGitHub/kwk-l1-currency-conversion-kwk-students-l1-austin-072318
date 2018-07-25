419e2b076ba892cd5491fe713e1889bc4f17c7b2
ETSGitHub <etschulte@gmail.com>
2018-07-25 16:41:54 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,36 @@
-# Write your cod here
+# Write your code here
+
+def usd_to_eur(usd="10")
+  usd * 0.86 
+end
+
+def eur_to_usd(eur)
+  eur / 0.86 
+end
+
+def usd_to_jpy(usd)
+  usd * 111.08
+end
+
+def jpy_to_usd(jpy)
+  jpy / 111.08
+end
+
+def usd_to_gbp
+  usd * 0.76
+end
+
+def gbp_to_usd
+  bgp / 0.76
+end
+
+def usd_to_aud
+  usd * 1.35
+end
+
+def aud_to_usd
+  aud / 1.35
+end
+
+
+
