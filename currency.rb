9940c926534389ffab70f5ceef8aaba73f22bed8
ETSGitHub <etschulte@gmail.com>
2018-07-25 16:51:18 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -50,27 +50,39 @@ choice = gets.chomp().to_i
 case choice
 when 1
   def usd_to_eur(usd="10")
-  usd * 0.86 
+  puts usd * 0.86 
   end
 when 2
   def usd_to_jpy(usd)
-  usd * 111.08
+  puts usd * 111.08
   end
 end
 when 3
   def usd_to_gbp(usd)
-  usd * 0.76
+  puts usd * 0.76
   end
 when 4
   def usd_to_aud(usd)
-  usd * 1.35
+  puts usd * 1.35
   end
 when 5
   def eur_to_usd(eur)
-  eur / 0.86 
+  puts eur / 0.86 
   end
 when 6
   def jpy_to_usd(jpy)
-  jpy / 111.08
+  puts jpy / 111.08
+  end
+when 7
+  def gbp_to_usd(gbp)
+  puts gbp / 0.76
+  end
+when 8
+  def aud_to_usd(aud)
+  puts aud / 1.35
+  end
+else 
+  puts "Invalid input, exiting..."
+  
 end
-
+  
